a5e2d2e8113e3687233226791b3ff334554992b5
Plip101 <Bazman15@gmail.com>
2016-10-12 12:58:28 +0100

    Progress Bar

---
+++ GameProject/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ProgressBar : MonoBehaviour {
+	public Image background;
+	public Text Txtprogress;
+	[Range(0,100)]
+	public int Amount = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void OnValidate()
+	{
+		float amount = (Amount / 100.0f);
+
+		background.fillAmount = amount;
+		Txtprogress.text = string.Format ("{0} %", Amount);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
