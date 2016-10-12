90953301f58cdd55ce8157bc9054b46eeebbd202
Plip101 <Bazman15@gmail.com>
2016-10-12 12:33:56 +0100

    BarryBranch
    BarryBranch Created

--- GameProject/Assets/Scripts/MoveForward.cs
+++ GameProject/Assets/Scripts/MoveForward.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MoveForward : MonoBehaviour {
 
 	public static float speed = 30.0f;
-	public static float standardSpeed = 50.0f;
+	public static float standardSpeed = 100.0f;
 	public  float acceleration = 0.1f;
 	
 	// Update is called once per frame
@@ -13,20 +13,20 @@ public class MoveForward : MonoBehaviour {
 		if (speed < standardSpeed) {
 			speed  += acceleration;			//Increasing speed.
 		}
-		if (speed > standardSpeed) {
-			speed -= 0.2f;						//If speed is above the standardSpeed, deccelerate until it is not.
-		}
+
 
 		transform.position += transform.forward*speed*Time.deltaTime;		//Actual movement.
 		if (speed >= 50) {
-			print("PRESS SPACE TO WARP");
 		}
 	}
 
 	public static void SpeedRingBoost() 
 	{
 		Debug.Log ("BOOST IN SPEED!");
-		speed += 90.0f;
+		speed += 50.0f;
+		ShipMovement.movementSpeed += 1.0f;
+		print (ShipMovement.movementSpeed);
+		print (speed);
 	}
 
 }
