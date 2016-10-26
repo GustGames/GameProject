d53eb25ad2be3bfa99cbcd8ae40039dc4884d040
BrendanPotts <pottsbrendan@rocketmail.com>
2016-10-26 11:53:30 +0100

    Added new cillision script

--- GameProject/Assets/Scripts/CillisonScript.cs
+++ GameProject/Assets/Scripts/CillisonScript.cs
@@ -2,6 +2,26 @@
 using System.Collections;
 
 public class CillisonScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		//Destroy (other.gameObject);
+		if (other.gameObject.name == "Astroid(Clone)") {
+			Debug.Log ("FUCK YOUR DREAMS");
+			Destroy (other.gameObject);
+			Debug.Log ("SPEED DOWN");
+			FOV.SpeedDown ();
+			MoveForward.SpeedAstroidDown ();
+		} else if (other.gameObject.name == "SpeedRing(Clone)") {
+			Debug.Log ("FUCK YOUR DREAMS");
+			Destroy (other.gameObject);
+			Debug.Log ("SPEED BOOST");
+			FOV.SpeedBoost ();
+			MoveForward.SpeedRingBoost ();
+		} else {
+			Debug.Log ("No Collision");
+		}
+	}
+	/**
 	void OnCollisionEnter ( Collision collision ){
 		Debug.Log ("Enter Called");
 
@@ -19,5 +39,5 @@ public class CillisonScript : MonoBehaviour {
 			FOV.SpeedDown();
 			MoveForward.SpeedAstroidDown();
 		}
-	}
+	}**/
 }
