d9b571345fd2280b21bbaf1644069d0cb5fc960c
Plip101 <Bazman15@gmail.com>
2016-11-23 12:53:31 +0000

    Feed Back Improvements + EMP done
    Feed Back Improvements + EMP done

---
+++ GameProject/Assets/Scripts/EmpCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmpCollision : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		//Destroy (other.gameObject);
+		if (other.gameObject.name == "Ship") {
+			FOV.SpeedDown ();
+			MoveForward.SpeedAstroidDown ();
+
+		} 
+		
+	}
+}
--- GameProject/Assets/Scripts/PowerUps.cs
+++ GameProject/Assets/Scripts/PowerUps.cs
@@ -13,7 +13,9 @@ public class PowerUps : MonoBehaviour {
 	public float amount;
 	public GameObject Ship;
 	public GameObject Blast;
+	public GameObject Emp;
 	public bool create = false;
+	public bool grow = false;
 	// Use this for initialization
 	void Start () {
 		speed = 100;
@@ -25,11 +27,12 @@ public class PowerUps : MonoBehaviour {
 		Txtprogress.text = string.Format ("{0} %", percent);
 		FillBar ();
 		Shield ();
+		EMP ();
 
 	}
 	void Shield (){
 		
-		if (percent > 0 && Input.GetKey ("space")) {
+		if (percent > 0 && Input.GetKey ("q")) {
 			
 			percent -= 1;
 			speed -= 1;
@@ -50,9 +53,34 @@ public class PowerUps : MonoBehaviour {
 		} else {
 			Debug.Log("done");
 		}
-		 
-
-
+	}
+	void EMP()
+	{
+		if (percent == 100 && Input.GetKeyDown("e"))
+		{
+			percent = 0;
+			speed = 0;
+			grow = true;
+			//background.fillAmount = amount;
+			//Txtprogress.text = string.Format ("{0} %", percent);
+			if (create == false)
+			{
+				GameObject emp = Instantiate (Emp, new Vector3 (Ship.transform.position.x, Ship.transform.position.y, Ship.transform.position.z + 1), Quaternion.Euler (0, 0, 0)) as GameObject;
+				emp.transform.parent = GameObject.Find ("Ship").transform;
+				create = true;
+			}
+		 	
+		}
+		if (grow == true) {
+			GameObject.Find ("EMP(Clone)").transform.localScale += new Vector3 (0.4f, 0.4f, 0.4f);
+			if (GameObject.Find ("EMP(Clone)").transform.localScale.y  >= 20)
+			{
+				Destroy (GameObject.Find ("EMP(Clone)"));
+				create = false;
+				grow = false;
+			}
+				
+		}
 	}
 	void FillBar()
 	{
